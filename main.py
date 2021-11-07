e04da9cc8e7e8e0083755192c03b21abffb8986b
Pavel Kurmyza <tmrrwnxtsn@gmail.com>
2021-11-07 13:22:55 +0400

    Implement content reading .doc(x) files and output it

--- main.py
+++ main.py
@@ -1,16 +1,44 @@
-# This is a sample Python script.
-
-# Press Shift+F17 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import docx
+from docx.opc.exceptions import PackageNotFoundError
+import sys
+import re
+import os
+import win32com.client as win32
+from win32com.client import constants
 
 
-# Press the green button in the gutter to run the script.
+# Converts .doc file to a file with the .docx extension
+def save_as_docx(doc_path: str) -> str:
+    # Opening MS Word
+    word = win32.gencache.EnsureDispatch('Word.Application')
+    word_doc = word.Documents.Open(doc_path)
+    word_doc.Activate()
+
+    # Rename doc_path file with .docx
+    new_file_abs = os.path.abspath(doc_path)
+    new_file_abs = re.sub(r'\.\w+$', '.docx', new_file_abs)
+
+    # Save and Close
+    word.ActiveDocument.SaveAs(new_file_abs, FileFormat=constants.wdFormatXMLDocument)
+    word_doc.Close(False)
+
+    return new_file_abs
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    try:
+        doc = docx.Document(sys.argv[1])
+    except PackageNotFoundError:
+        print(f'File not found at "{sys.argv[1]}"')
+        exit(1)
+    except ValueError:
+        # ValueError means that input file with .doc extension
+        doc_abspath = os.path.abspath(sys.argv[1])
+        # Converts .doc to .docx
+        docx_path = save_as_docx(doc_abspath)
+        doc = docx.Document(docx_path)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    for paragraph in doc.paragraphs:
+        print(paragraph.text)
+
+    exit(0)
