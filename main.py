20f560df3c40385f1c3e80ff38520dd0bc1cd270
Pavel Kurmyza <tmrrwnxtsn@gmail.com>
2021-11-22 20:20:28 +0400

    Add database interaction

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
 import sys
 from docx import Document
 from os.path import basename, splitext, abspath
-from json import dump
+from os import getenv
 from docx.opc.exceptions import PackageNotFoundError
 from word import DocxAnalyzer
+from pymongo import MongoClient
+from dotenv import load_dotenv
+
+load_dotenv()
 
 if __name__ == "__main__":
     try:
@@ -16,8 +20,11 @@ if __name__ == "__main__":
         analyzer = DocxAnalyzer(document)
         document_structure = analyzer.get_docx_structure()
 
-        with open(f"out/{splitext(basename(path))[0]}.json", "w", encoding='UTF8') as file:
-            dump(document_structure, file, ensure_ascii=False)
+        client = MongoClient(getenv("CONNECTION_STRING"))
+        db = client["documentsAnalysis"]
+        coll = db["techSpecifications"]
+
+        coll.insert_one({"document_name": splitext(basename(path))[0], "structure": document_structure})
     except PackageNotFoundError:
         print(f"File not found at {sys.argv[1]}")
         exit(1)
