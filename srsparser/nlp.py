640a3c9b3bfd80ebbb7f46a571a46e7a57c8291a
Pavel Kurmyza <tmrrwnxtsn@gmail.com>
2022-01-14 14:51:51 +0400

    Add Gensim library and more NLP functions

--- srsparser/nlp.py
+++ srsparser/nlp.py
@@ -1,24 +1,42 @@
-from re import sub
-
 from pymorphy2 import MorphAnalyzer
+from gensim.utils import simple_preprocess
 
 from srsparser import configs
 
 morph = MorphAnalyzer()
 
 
-def lemmatize(s: str) -> list:
+def remove_stop_words(words: list) -> list:
     """
-    Replaces all the word forms in the string with the initial forms.
+    Remove stopwords from words.
+
+    :param words: word list.
+    :return: word list without stopwords.
     """
-    s = sub(configs.EXCESS_CHARS, "", s)
-    tokens = []
-    for token in s.split():
-        if token and token not in configs.STOPWORDS_RU:
-            token = token.strip()
-            token = morph.normal_forms(token)[0]
-            tokens.append(token)
-    return tokens
+    return list(filter(lambda token: token not in configs.STOPWORDS_RU, words))
+
+
+def lemmatize(words: list) -> list:
+    """
+    Brings each word to its normal dictionary form.
+
+    :param words: word list.
+    :return: lemmatized word list.
+    """
+    return list(map(lambda token: morph.normal_forms(token)[0], words))
+
+
+def tokenize(doc: str) -> list:
+    """
+    Preprocessing of a text document to obtain a list of tokens.
+
+    :param doc: some document.
+    :return: token list.
+    """
+
+    tokens = simple_preprocess(doc, min_len=2, max_len=50, deacc=True)
+    tokens = remove_stop_words(tokens)
+    return lemmatize(tokens)
 
 
 def strings_similarity(s1: str, s2: str) -> float:
@@ -28,26 +46,26 @@ def strings_similarity(s1: str, s2: str) -> float:
     :return: 0.0 — completely dissimilar, 1.0 — equal.
     """
     # tokenization
-    x_list = lemmatize(s1.lower())
-    y_list = lemmatize(s2.lower())
+    s1_list = tokenize(s1)
+    s2_list = tokenize(s2)
 
-    if not x_list or not y_list:
+    if not s1_list or not s2_list:
         return 0.0
 
     l1 = []
     l2 = []
 
-    x_set = {w for w in x_list}
-    y_set = {w for w in y_list}
+    s1_set = {w for w in s1_list}
+    s2_set = {w for w in s2_list}
 
     # form a set containing keywords of both strings
-    rvector = x_set.union(y_set)
+    rvector = s1_set.union(s2_set)
     for w in rvector:
-        if w in x_set:
+        if w in s1_set:
             l1.append(1)  # create a vector
         else:
             l1.append(0)
-        if w in y_set:
+        if w in s2_set:
             l2.append(1)
         else:
             l2.append(0)
