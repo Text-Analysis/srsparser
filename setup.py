66137193bfd498a30e1eba2644d1704955b3e065
tmrrwnxtsn <tmrrwnxtsn@gmail.com>
2022-04-27 02:45:18 +0400

    Increase version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open('README.md', 'r', encoding='utf-8') as file:
 
 setup(
     name='srsparser',
-    version='1.4.6',
+    version='1.4.7',
     author='Kurmyza Pavel',
     author_email='tmrrwnxtsn@gmail.com',
     project_urls={
